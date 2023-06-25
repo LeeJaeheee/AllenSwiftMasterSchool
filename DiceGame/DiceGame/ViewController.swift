ec75096a54156c278dd66ce4c99affef209a15f4
LeeJaeheee <sns3434@gmail.com>
2023-06-25 15:20:11 +0900

    [Dice Game]

---
+++ DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by 이재희 on 2023/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    let diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        // 첫 번째 이미지뷰의 이미지를 랜덤으로 변경
+        firstImageView.image = diceArray.randomElement()
+        // 두 번째 이미지뷰의 이미지를 랜덤으로 변경
+        secondImageView.image = diceArray.randomElement()
+    }
+    
+    
+}
+
