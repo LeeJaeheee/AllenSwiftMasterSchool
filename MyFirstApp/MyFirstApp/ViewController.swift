f8ba5cdfe3a26c15e4294a04bd69cf7d926d2f85
LeeJaeheee <sns3434@gmail.com>
2023-06-25 12:45:29 +0900

    Feat: 버튼 클릭 시 버튼 색상 변경 코드 추가

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var myButton: UIButton!
     
     // 앱의 화면에 들어오면 처음 실행시키는 함수
     override func viewDidLoad() {
@@ -23,6 +24,8 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         mainLabel.text = "안녕하세요"
+        myButton.backgroundColor = UIColor.yellow
+        myButton.setTitleColor(.black, for: .normal)
         
 //         방법 1: #colorLiteral( 입력하면 전환됨
 //        mainLabel.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
